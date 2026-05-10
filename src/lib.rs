53b0ed1b4a73ed757a267024d4601fc562415e42
Viktor Ku <viktor.konsta@gmail.com>
2026-05-10 15:18:17 +0300

    start to integrate the sketch into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
-// humble beginnings
+mod arr;
+mod path;
+mod path_bytes;
+mod path_group;
--- src/path/mod.rs
+++
@@ -1,7 +0,0 @@
-mod arr;
-mod path;
-mod path_bytes;
-mod path_group;
-
-pub use path::Path;
-pub use path_group::PathGroup;
